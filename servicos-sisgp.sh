d4c3715995fff1d336c6d4aa32a928b78eee2c20
uai21 <mauricio@uai21.com.br>
2022-12-27 09:50:22 -0300

    Ajustes dos scripts

--- servicos-sisgp.sh
+++ servicos-sisgp.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-## VERIFICAÇÃO DE SERVIÇOS DO SISGP
-## PARA RODAR EM ROTINA DE MANUTENÇÃO
 
 # Verifica a opção passada
 case "$1" in
@@ -11,8 +9,13 @@ case "$1" in
     echo "Status do serviço nginx: $status_nginx"
     status_freeradius=$(systemctl is-active freeradius)
     echo "Status do serviço freeradius: $status_freeradius"
+    status_mariadb=$(systemctl is-active mariadb)
+    echo "Status do serviço mariadb: $status_mariadb"
+    status_sisgp=$(systemctl is-active sisgp)
+    echo "Status do serviço sisgp: $status_sisgp"
     ;;
-  # Ativa os serviços
+    # Ativa os serviços
+ 
   --ativar)
     echo "Ativando serviços..."
     if systemctl is-active nginx | grep -q 'inactive'; then
@@ -23,8 +26,17 @@ case "$1" in
       systemctl start freeradius
       echo "Serviço freeradius ativado com sucesso"
     fi
+    if systemctl is-active mariadb | grep -q 'inactive'; then
+      systemctl start mariadb
+      echo "Serviço mariadb ativado com sucesso"
+    fi
+    if systemctl is-active sisgp | grep -q 'inactive'; then
+      systemctl start sisgp
+      echo "Serviço sisgp ativado com sucesso"
+    fi
     ;;
-  # Para os serviços
+ 
+# Para os serviços
   --parar)
     echo "Parando serviços..."
     if systemctl is-active nginx | grep -q 'active'; then
@@ -35,10 +47,30 @@ case "$1" in
       systemctl stop freeradius
       echo "Serviço freeradius parado com sucesso"
     fi
+    if systemctl is-active mariadb | grep -q 'active'; then
+      systemctl stop mariadb
+      echo "Serviço mariadb parado com sucesso"
+    fi
+    if systemctl is-active sisgp | grep -q 'active'; then
+      systemctl stop sisgp
+      echo "Serviço sisgp parado com sucesso"
+    fi
+    ;;
+  # Exibe a ajuda
+  --ajuda|-h)
+    echo "Uso: servicos-sisgp.sh [OPÇÃO]"
+    echo "Opções disponíveis:"
+    echo "  --verifica   Verifica o status dos serviços"
+    echo "  --ativar     Ativa os serviços que estiverem parados"
+    echo "  --parar      Para todos os serviços que estiverem ativos"
+    echo "  --ajuda,-h   Exibe esta mensagem de ajuda"
+    exit 0
     ;;
   # Opção inválida
   *)
-    echo "Opção inválida. Use --verifica para verificar o status dos serviços, --ativar para ativar os serviços ou --parar para parar os serviços."
+    echo "Opção inválida. Use --ajuda ou -h para exibir a lista de opções disponíveis."
     exit 1
     ;;
 esac
+
+exit 0
